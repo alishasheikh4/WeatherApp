80fbe37f0c45c9367b2776ea418b682d1b168662
Alisha Sheikh <alishasheikh337@gmail.com>
2025-11-07 16:09:29 +0530

    Initial commit - Weather project

---
+++ weather_project/weather/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Weather(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=200)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    icon = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.city} - {self.temperature}°C"
---
+++ weather_project/weather/views.py
@@ -0,0 +1,35 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from .models import Weather
+from .serializers import WeatherSerializer
+
+@api_view(['GET'])
+def get_weather(request):
+    city = request.GET.get('city')
+    if not city:
+        return Response({'error': 'City parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    api_key = "3ebf7b2aa3d5a7151fbfd713d1fd52f0"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        return Response({'error': 'City not found or API error'}, status=response.status_code)
+
+    data = response.json()
+
+    weather_data = {
+        'city': data['name'],
+        'temperature': data['main']['temp'],
+        'description': data['weather'][0]['description'],
+        'country': data['sys']['country'],
+        'icon': data['weather'][0]['icon']
+    }
+
+    # Save to database
+    weather_obj = Weather.objects.create(**weather_data)
+
+    serializer = WeatherSerializer(weather_obj)
+    return Response(serializer.data)
